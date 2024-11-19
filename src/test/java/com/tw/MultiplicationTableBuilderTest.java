c819ed8d9f71178aa8db749c896d6bf277e544e8
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-19 10:35:51 +0800

    test: add test cases for isInRange function with input as 0 and 1001

--- src/test/java/com/tw/MultiplicationTableBuilderTest.java
+++ src/test/java/com/tw/MultiplicationTableBuilderTest.java
@@ -2,6 +2,7 @@ package com.tw;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MultiplicationTableBuilderTest {
@@ -19,4 +20,29 @@ class MultiplicationTableBuilderTest {
     }
 
     // TODO: add more tests for isInRange
+    @Test
+    void should_return_false_when_isInRange_given_a_number_0() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int number = 0;
+
+        // When
+        boolean isInRange = multiplicationTableBuilder.isInRange(number);
+
+        // Then
+        assertFalse(isInRange);
+    }
+
+    @Test
+    void should_return_false_when_isInRange_given_a_number_1001() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int number = 1001;
+
+        // When
+        boolean isInRange = multiplicationTableBuilder.isInRange(number);
+
+        // Then
+        assertFalse(isInRange);
+    }
 }
